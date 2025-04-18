4708013bb06b446b7711279d87343eb997010a1f
Nochen0 <enesaydindev@gmail.com>
2025-04-18 20:32:46 +0300

    wrote retry with recursion

--- src/Observable.ts
+++ src/Observable.ts
@@ -644,31 +644,24 @@ export default class Observable<T> {
 
   public retry(this: Observable<T>, times: number) {
     return new Observable<T>((subscriber) => {
-      let currentSubscription: undefined | Subscription
-      let successfull = false
-      ;(async () => {
-        for (let i = 0; i < times; i++) {
-          if (successfull) break
-          await new Promise<void>((resolve) => {
-            currentSubscription = this.subscribe({
-              next(x) {
-                subscriber.next(x)
-              },
-              complete() {
-                successfull = true
-                subscriber.complete()
-                currentSubscription!.unsubscribe()
-                resolve()
-              },
-              error(e) {
-                subscriber.error(e)
-                currentSubscription!.unsubscribe()
-                resolve()
-              },
-            })
-          })
-        }
-      })()
+      const self = this
+      const subscription = this.subscribe({
+        next(x) {
+          subscriber.next(x)
+        },
+        complete() {
+          subscriber.complete()
+          subscription.unsubscribe()
+        },
+        error(e) {
+          if (times < 0) {
+            subscriber.error(e)
+            subscription.unsubscribe()
+            return
+          }
+          self.retry(times - 1)
+        },
+      })
     })
   }
 }
